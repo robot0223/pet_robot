7ed1c245b914780dd5a499b58c208a49cb7028d6
robot0223 <130275185+robot0223@users.noreply.github.com>
2023-05-13 21:57:49 +0900

    adding things
    new functions and huskeylens set.

--- vector_bf/vector_bf.ino
+++ vector_bf/vector_bf.ino
@@ -1,15 +1,15 @@
 //libraries
-#include<Arduino.h>
-#include<SoftwareSerial.h>
-#include<Mecha_VoiceRecognition.h>
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <Mecha_VoiceRecognition.h>
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9340.h>
-#include<Servo.h>
+#include <Servo.h>
 #include <HUSKYLENS.h>
 
 //voice
-SoftwareSerial uart_voice(10, 11);
+SoftwareSerial uart_voice(10, 11);  //RX, TX
 Mecha_VoiceRecognition voice(&uart_voice);
 
 //tft lcd
@@ -17,6 +17,15 @@ Mecha_VoiceRecognition voice(&uart_voice);
 #undef __FlashStringHelper::F(string_literal)
 #define F(string_literal) string_literal
 #endif
+
+//hardware spi communication
+Adafruit_ILI9340 tft = Adafruit_ILI9340(CS, DC, TFT_RST);
+
+//Huskeylens
+HUSKYLENS huskylens;
+SoftwareSerial mySerial(, );               // RX, TX
+void huskeylens_printResult(HUSKYLENSResult result);  //function definition
+
 
 //servo moter
 Servo serv;
@@ -36,27 +45,32 @@ Servo serv;
 #define MOTOR_B1 4
 #define MOTOR_B2 5
 
+//etc. pin
+
+
+
 //voltage
-#define V3_3 120
+#define TFT__V 120
 
 
 
 
 
 
-//hardware spi communication
-Adafruit_ILI9340 tft = Adafruit_ILI9340(CS, DC, TFT_RST);
+
 
 void setup() {
-
+  //Serial
   Serial.begin(9600);
+  Serial.begin(115200);
 
-
+  //pin settings
   pinMode(MOTOR_A1, OUTPUT);
   pinMode(MOTOR_B1, OUTPUT);
   pinMode(MOTOR_A2, OUTPUT);
   pinMode(MOTOR_B2, OUTPUT);
   pinMode(TFTLED, OUTPUT);
+  
   //voice
   voice.setDebugOn(&Serial);
   voice.setGroup(VOICE_GROUP_1);
@@ -66,9 +80,9 @@ void setup() {
   //servo
   //serv.attach(SERVO_MOTOR);
 
-  
+
   //tft lcd
-  analogWrite(TFTLED, V3_3);
+  analogWrite(TFTLED, TFT_V);
   tft.begin();
   tft.fillScreen(ILI9340_BLACK);
   Serial.println(closed_eye());
@@ -77,23 +91,56 @@ void setup() {
 void loop() {
   //get voice memory
   VOICE_InstructionTypeDef response = voice.getInstruction();
-  if (response == VOICE_INSTRUCTION_1 || response == VOICE_INSTRUCTION_2 || response == VOICE_INSTRUCTION_3 || response == VOICE_INSTRUCTION_4 || response == VOICE_INSTRUCTION_5)
-  {
-    Serial.println("dkdk");
+  if (response == VOICE_INSTRUCTION_1 || response == VOICE_INSTRUCTION_2 || response == VOICE_INSTRUCTION_3 || response == VOICE_INSTRUCTION_4 || response == VOICE_INSTRUCTION_5) {
+   //call function related to 'voicedetact-reaction'
   }
+
+
+
 
 
 }
 
-int forward(int mot1speed, int mot2speed)
-{
+
+//dc motor movement functions
+void forward(int mot1speed, int mot2speed) {
   analogWrite(MOTOR_A1, mot1speed);
   analogWrite(MOTOR_B1, mot2speed);
 }
 
-unsigned long eye()
-{
-  tft.fillRoundRect(50, 40, 100, 100, 30, tft.Color565(0, 100, 0));
+void backwards(int mot1speed, int mot2speed) {
+analogWrite(MOTOR_A2);
+analogWrite(MOTOR_B2);
+}
+
+void leftspin(int mot1speed, int mot2speed){
+analogWrite(MOTOR_A2);
+analogWrite(MOTOR_B1);
+}
+
+void rightspin(int mot1speed, int mot2speed){
+analogWrite(MOTOR_A1);
+analogWrite(MOTOR_B2);
+}
+
+
+
+//huskeylens functions
+void huskeylens_printResult(HUSKYLENSResult result) {
+  if (result.command == COMMAND_RETURN_BLOCK) {
+    Serial.println(String() + F("Block:xCenter=") + result.xCenter + F(",yCenter=") + result.yCenter + F(",width=") + result.width + F(",height=") + result.height + F(",ID=") + result.ID);
+  } else if (result.command == COMMAND_RETURN_ARROW) {
+    Serial.println(String() + F("Arrow:xOrigin=") + result.xOrigin + F(",yOrigin=") + result.yOrigin + F(",xTarget=") + result.xTarget + F(",yTarget=") + result.yTarget + F(",ID=") + result.ID);
+  } else {
+    Serial.println("Object unknown!");
+  }
+}
+
+
+
+// tft lcd functions
+unsigned long eye() {
+  tft.fillRoundRect(50, 40, 100, 100, 30, tft.Color565(0, 100, 0));//tft.Color565(red,green,blue);
   tft.fillRoundRect(50, 180, 100, 100, 30, tft.Color565(0, 100, 0));
   tft.fillRoundRect(50, 40, 95, 95, 30, tft.Color565(0, 150, 0));
   tft.fillRoundRect(50, 180, 95, 95, 30, tft.Color565(0, 150, 0));
@@ -103,10 +150,14 @@ unsigned long eye()
 
 
 
-unsigned long closed_eye()
-{
+unsigned long closed_eye() {
   tft.fillRoundRect(55, 45, 90, 90, 30, tft.Color565(0, 0, 0));
   tft.fillRoundRect(55, 185, 90, 90, 30, tft.Color565(0, 0, 0));
   tft.fillRoundRect(50, 40, 90, 90, 30, tft.Color565(0, 240, 0));
   tft.fillRoundRect(50, 180, 90, 90, 30, tft.Color565(0, 240, 0));
 }
+
+
+
+
+
