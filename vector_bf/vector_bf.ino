2c4c6eef846e3d2d6ff5926b0271e5f664d6456b
robot0223 <130275185+robot0223@users.noreply.github.com>
2023-05-14 14:37:50 +0900

    sdaasds
    addaasdWREDASFE

--- vector_bf/vector_bf.ino
+++ vector_bf/vector_bf.ino
@@ -8,8 +8,43 @@
 #include <Servo.h>
 #include <HUSKYLENS.h>
 
+//voice pin
+#define VOICE_RX 10
+#define VOICE_TX 11
+
+//bluetooth pin
+#define BLUETOOTH_RX 14
+#define BLUETOOTH_TX 15
+
+//huskeylens pin
+#define HUSKEYLENS_RX 12
+#define HUSKEYLENS_TX 13
+//tft lcd pin
+#define SCLK 52
+#define MISO 50
+#define MOSI 51
+#define CS 53
+#define DC 48
+#define TFT_RST 49
+#define TFT_LED 6
+
+//moter pin
+#define MOTOR_A1 2
+#define MOTOR_A2 3
+#define MOTOR_B1 4
+#define MOTOR_B2 5
+#define SERVO 24
+//etc. pin
+#define BUZZER 22
+#define BUTTON 23
+
+
+//voltage
+#define TFT__V 120
+
+
 //voice
-SoftwareSerial uart_voice(10, 11);  //RX, TX
+SoftwareSerial uart_voice(VOICE_RX, VOICE_TX);  //RX, TX
 Mecha_VoiceRecognition voice(&uart_voice);
 
 //tft lcd
@@ -23,40 +58,13 @@ Adafruit_ILI9340 tft = Adafruit_ILI9340(CS, DC, TFT_RST);
 
 //Huskeylens
 HUSKYLENS huskylens;
-SoftwareSerial mySerial(, );               // RX, TX
-void huskeylens_printResult(HUSKYLENSResult result);  //function definition
-
+SoftwareSerial mySerial(, );  // RX, TX
 
 //servo moter
 Servo serv;
 
-//tft lcd pin
-#define SCLK 52
-#define MISO 50
-#define MOSI 51
-#define CS 53
-#define DC 48
-#define TFT_RST 49
-#define TFTLED 6
-
-//moter pin
-#define MOTOR_A1 2
-#define MOTOR_A2 3
-#define MOTOR_B1 4
-#define MOTOR_B2 5
-
-//etc. pin
-
-
-
-//voltage
-#define TFT__V 120
-
-
-
-
-
-
+//bluetooth
+SoftwareSerial bluetooth(BLUETOOTH_RX, BLUETOOTH_TX);
 
 
 void setup() {
@@ -65,12 +73,19 @@ void setup() {
   Serial.begin(115200);
 
   //pin settings
+  //dc motor
   pinMode(MOTOR_A1, OUTPUT);
   pinMode(MOTOR_B1, OUTPUT);
   pinMode(MOTOR_A2, OUTPUT);
   pinMode(MOTOR_B2, OUTPUT);
+
+  //tft lcd
   pinMode(TFTLED, OUTPUT);
-  
+
+//etcf.
+pinMode(BUZZER,OUTPUT);
+pinMode(BUTTON,INPUT);
+
   //voice
   voice.setDebugOn(&Serial);
   voice.setGroup(VOICE_GROUP_1);
@@ -78,28 +93,31 @@ void setup() {
   voice.setMode(VOICE_MODE_RECOGNITION);
 
   //servo
-  //serv.attach(SERVO_MOTOR);
+  serv.attach(SERVO);
 
 
   //tft lcd
-  analogWrite(TFTLED, TFT_V);
+  analogWrite(TFT_LED, TFT_V);
   tft.begin();
   tft.fillScreen(ILI9340_BLACK);
   Serial.println(closed_eye());
 }
 
 void loop() {
-  //get voice memory
-  VOICE_InstructionTypeDef response = voice.getInstruction();
-  if (response == VOICE_INSTRUCTION_1 || response == VOICE_INSTRUCTION_2 || response == VOICE_INSTRUCTION_3 || response == VOICE_INSTRUCTION_4 || response == VOICE_INSTRUCTION_5) {
-   //call function related to 'voicedetact-reaction'
-  }
+  
+//if button is pressed
+//turn robot into wake mode(servo on, lcd on 
+//lcd print download robot app
+//bluetooth connection with phone 
+//if connected lcd print enter code ****
+
 
 
 
 
 
 }
+
 
 
 //dc motor movement functions
@@ -109,21 +127,36 @@ void forward(int mot1speed, int mot2speed) {
 }
 
 void backwards(int mot1speed, int mot2speed) {
-analogWrite(MOTOR_A2);
-analogWrite(MOTOR_B2);
+  analogWrite(MOTOR_A2);
+  analogWrite(MOTOR_B2);
 }
 
-void leftspin(int mot1speed, int mot2speed){
-analogWrite(MOTOR_A2);
-analogWrite(MOTOR_B1);
+void leftspin(int mot1speed, int mot2speed) {
+  analogWrite(MOTOR_A2);
+  analogWrite(MOTOR_B1);
 }
 
-void rightspin(int mot1speed, int mot2speed){
-analogWrite(MOTOR_A1);
-analogWrite(MOTOR_B2);
+void rightspin(int mot1speed, int mot2speed) {
+  analogWrite(MOTOR_A1);
+  analogWrite(MOTOR_B2);
 }
 
+//voice recongniton functions
+void voicerecongnition_response() {
+  VOICE_InstructionTypeDef response = voice.getInstruction();
+  if (response == VOICE_INSTRUCTION_1 || response == VOICE_INSTRUCTION_2 || response == VOICE_INSTRUCTION_3 || response == VOICE_INSTRUCTION_4 || response == VOICE_INSTRUCTION_5) {
 
+    voice.setGroup(VOICE_GROUP_2);
+    VOICE_InstructionTypeDef response = voice.getInstruction();
+
+    switch (response) {
+      case VOICE_INSTRUCTION_1:
+      Serial.println("1");
+      break;
+    }
+
+  }
+}
 
 //huskeylens functions
 void huskeylens_printResult(HUSKYLENSResult result) {
@@ -136,11 +169,9 @@ void huskeylens_printResult(HUSKYLENSResult result) {
   }
 }
 
-
-
 // tft lcd functions
 unsigned long eye() {
-  tft.fillRoundRect(50, 40, 100, 100, 30, tft.Color565(0, 100, 0));//tft.Color565(red,green,blue);
+  tft.fillRoundRect(50, 40, 100, 100, 30, tft.Color565(0, 100, 0));  //tft.Color565(red,green,blue);
   tft.fillRoundRect(50, 180, 100, 100, 30, tft.Color565(0, 100, 0));
   tft.fillRoundRect(50, 40, 95, 95, 30, tft.Color565(0, 150, 0));
   tft.fillRoundRect(50, 180, 95, 95, 30, tft.Color565(0, 150, 0));
@@ -148,16 +179,9 @@ unsigned long eye() {
   tft.fillRoundRect(55, 185, 90, 90, 30, tft.Color565(0, 240, 0));
 }
 
-
-
 unsigned long closed_eye() {
   tft.fillRoundRect(55, 45, 90, 90, 30, tft.Color565(0, 0, 0));
   tft.fillRoundRect(55, 185, 90, 90, 30, tft.Color565(0, 0, 0));
   tft.fillRoundRect(50, 40, 90, 90, 30, tft.Color565(0, 240, 0));
   tft.fillRoundRect(50, 180, 90, 90, 30, tft.Color565(0, 240, 0));
 }
-
-
-
-
-
